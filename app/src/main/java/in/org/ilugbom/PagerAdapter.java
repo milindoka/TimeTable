6b0577b9e6875c6f9afe58d1fd7a8a646e9fcd75
milindoka <oak445@gmail.com>
2020-03-01 23:53:44 +0530

    Set Text in Table Cell By Alert Dialog Edit Text

--- app/src/main/java/in/org/ilugbom/PagerAdapter.java
+++ app/src/main/java/in/org/ilugbom/PagerAdapter.java
@@ -1,9 +1,13 @@
 package in.org.ilugbom;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -18,16 +22,94 @@ import java.util.zip.Inflater;
 public class PagerAdapter extends RecyclerView.Adapter {
     private List<PagerM> pagerMList;
 
-    class PagerViewHolder extends RecyclerView.ViewHolder {
+    class PagerViewHolder extends RecyclerView.ViewHolder  implements View.OnClickListener
+    {
 
         private Button btnReset;
+        private Button btnb00,btnb01,btnb02;
 
         public PagerViewHolder(@NonNull View itemView) {
             super(itemView);
 
             btnReset = itemView.findViewById(R.id.bReset);
+            btnb00  = itemView.findViewById(R.id.b00);
+            btnb00.setOnClickListener(this); // calling onClick() method
+
+            btnb01  = itemView.findViewById(R.id.b01);
+            btnb01.setOnClickListener(this); // calling onClick() method
+
+            btnb02  = itemView.findViewById(R.id.b02);
+            btnb02.setOnClickListener(this); // calling onClick() method
+
         }
-    }
+
+        @Override
+        public void onClick(View v)
+        {
+         // showForgotDialog(v.getContext());
+            final int V=v.getId();
+            final EditText taskEditText = new EditText(v.getContext());
+            AlertDialog dialog = new AlertDialog.Builder(v.getContext())
+                    .setTitle("Forgot Password")
+                    .setMessage("Enter your mobile number?")
+                    .setView(taskEditText)
+                    .setPositiveButton("Reset", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            String task = String.valueOf(taskEditText.getText());
+
+
+
+                            switch (V) {
+
+                                case R.id.b00:
+                                    btnb00.setText(task);
+                                    break;
+
+                                case R.id.b01:
+                                    btnb01.setText(task);
+                                    break;
+
+                                case R.id.b02:
+                                    btnb02.setText(task);
+                                    break;
+
+                                default:
+                                    break;
+                            }
+         }
+                    })
+                    .setNegativeButton("Cancel", null)
+                    .create();
+            dialog.show();
+
+
+
+        }
+
+
+         void showForgotDialog(Context c) {
+            final EditText taskEditText = new EditText(c);
+            AlertDialog dialog = new AlertDialog.Builder(c)
+                    .setTitle("Forgot Password")
+                    .setMessage("Enter your mobile number?")
+                    .setView(taskEditText)
+                    .setPositiveButton("Reset", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            String task = String.valueOf(taskEditText.getText());
+           //                 temp=task;
+                        }
+                    })
+                    .setNegativeButton("Cancel", null)
+                    .create();
+            dialog.show();
+        }
+
+
+
+
+    }  ////end of class pageviewholder
 
     public PagerAdapter(List<PagerM> pagerMList) {
         this.pagerMList = pagerMList;
